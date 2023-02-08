a9c4d57197b850cf642b77cb92a9999fc164d88c
Martin Kessler <martin.kessler@ares-consulting.de>
2023-02-08 09:32:37 +0100

    add cloud interview app

---
+++ cloud/python-app/src/app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, current_app as app
+
+import cpuinfo
+import psutil
+import platform
+import datetime
+import os
+
+
+@app.route("/")
+def index():    
+    user = os.environ.get("USERNAME", "")    
+    return render_template("index.html", username=user)
+
+@app.route("/info")
+def info():
+    osinfo = {}
+    osinfo["plat"] = platform
+    osinfo["cpu"] = cpuinfo.get_cpu_info()
+    osinfo["mem"] = psutil.virtual_memory()
+    osinfo["net"] = psutil.net_if_addrs()
+    osinfo["boottime"] = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    return render_template("info.html", info=osinfo)
+
+@app.route("/monitor")
+def monitor():
+    return render_template("monitor.html")
